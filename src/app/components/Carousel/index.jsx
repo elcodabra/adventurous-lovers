7316a73d2fc9ee9ed87c061f664d873e46b35c2f
akonovalov <akonovalov@at-consulting.ru>
2018-06-24 18:06:47 +0300

    upd Carousel

--- src/app/components/Carousel/index.jsx
+++ src/app/components/Carousel/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import SVG from 'react-inlinesvg';
+import LeftArrow from '../../../assets/img/left_arrow.svg';
+import RightArrow from '../../../assets/img/right_arrow.svg'
 
 import './styles.css';
 
 const Carousel = ({images}) => (
   <div className='carousel'>
-    {images && images.length > 0 && images.map((image, key) =>
-      <img className='carousel_img' key={key} src={image.src} />)
-    }
+    <SVG className='arrow_left' src={LeftArrow} />
+    <div className='carousel_content'>
+      {images && images.length > 0 && images.map((image, key) =>
+        <img className='carousel_img' key={key} src={image.src} />)
+      }
+    </div>
+    <SVG className='arrow_right' src={RightArrow} />
   </div>
 );
 
